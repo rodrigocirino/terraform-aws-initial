f7417e162d57e5ff72e15871ef7047892dc0fbc9
Rodrigo <you@example.com>
2021-03-04 16:20:34 -0300

    update versao4

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
   count = 3
   ami = "ami-026c8acd92718196b"
@@ -35,22 +41,19 @@ resource "aws_instance" "dev5" {
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
-resource "aws_security_group" "acesso-ssh" {
-  name        = "acesso-ssh"
-  description = "acesso-ssh"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["191.32.154.79/32"]
-  }
+resource "aws_instance" "dev6" {
+  provider = "aws.us-east-2"
+  ami = "ami-0d8f6eb4f641ef691"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
   tags = {
-    Name = "ssh"
+    Name = "dev6"
   }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+  depends_on = ["aws_dynamodb_table.dynamodb-homologacao"]
 }
+
+
 resource "aws_s3_bucket" "dev4" {
   bucket = "rmerceslabs-dev4"
   acl    = "private"
@@ -59,3 +62,22 @@ resource "aws_s3_bucket" "dev4" {
     Name = "rmerceslabs-dev4"
   }
 }
+
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider = "aws.us-east-2"
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+}
+
